a4b9a6cb31bc8f728cfce6e7fe295a22541992f4
animalize <wjsszsh@sohu.com>
2015-09-01 11:19:52 +0800

    增强检测新版本

--- tz2txt/checkver.py
+++ tz2txt/checkver.py
@@ -23,21 +23,21 @@ def main():
     except:
         raise Exception('无法用utf-8解码“版本发布网页”')
     
-    p = r'【最新版本】(.*?)【结束】'
-    m = re.search(p, html)
+    p = r'【最新版本】(.*?)【结束】.*?【更新网址】(.*?)【结束】'
+    m = re.search(p, html, re.DOTALL)
     if not m:
         raise Exception('无法从“版本发布网页”提取最新的版本号')
     
     newver = m.group(1)
+    download_url = m.group(2)
+    
     if newver == tz2txt_date:
         print('检测完毕，正在使用的是最新版。\n')
     else:   
         print('发现新版本：%s' % newver)
         
         answer = input('是否打开浏览器进入下载页面？(输入y打开，否则退出)')
-        if answer.strip().lower() == 'y':
-            download_url = 'http://pan.baidu.com/s/1ntwkwOD#path=%252Ftz2txt'
-            
+        if answer.strip().lower() == 'y':           
             try:
                 webbrowser.open_new_tab(download_url)
             except:
